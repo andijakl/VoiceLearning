ec4d837eeb8a71b4c757cf2aa296359bf428c2cf
Andreas Jakl <andreas.jakl@live.com>
2020-08-05 13:16:30 +0200

    Improve code organization

--- lambda/index.js
+++ lambda/index.js
@@ -6,6 +6,7 @@ const AWS = require('aws-sdk');
 const { DynamoDbPersistenceAdapter } = require('ask-sdk-dynamodb-persistence-adapter');
 const persistenceAdapter = new DynamoDbPersistenceAdapter({ tableName : process.env.DYNAMODB_TABLE_NAME });
 const dbHandler = require('./dbHandler.js');
+const trainingHandler = require('./trainingHandler.js');
 //import { getRequestType, getIntentName, getSlotValue, SkillBuilders } from 'ask-sdk-core';
 //import * as Alexa from 'ask-sdk-core'
 //import persistenceAdapter from 'ask-sdk-s3-persistence-adapter';
@@ -44,25 +45,10 @@ const LaunchRequestHandler = {
             speakOutput = "Hi and welcome to the learning asssistant! I can help you understand the most important concepts of your courses. First, please tell me your first name!"
             repromptOutput = "Please tell me your first name."
             // Initialize new user
-            persistentAttributes.currentCourse = null;
-            persistentAttributes.startedTrainings = 0;
-            persistentAttributes.finishedTrainings = 0;
-            persistentAttributes.totalQuestionsAsked = 0;
-            persistentAttributes.totalCorrectAnswers = 0;
-            persistentAttributes.totalWrongAnswers = 0;
-            sessionAttributes.state = states.STUDENT_NAME;
+            trainingHandler.initializeUser();
         }
 
-        //speakOutput += " I'm running node.js version: " + process.versions.node;
-        if (repromptOutput === null) {
-            repromptOutput = speakOutput;
-        }
-        
-        // Save state
-        persistentAttributes.repromptOutput = repromptOutput;
-        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-        handlerInput.attributesManager.setPersistentAttributes(persistentAttributes);
-        await handlerInput.attributesManager.savePersistentAttributes();
+        repromptOutput = await saveAttributes(speakOutput, repromptOutput, sessionAttributes, persistentAttributes, handlerInput);
 
         //const userId = handlerInput.requestEnvelope.session.user.userId;
         // TODO: save to DB - userId, so that we can track who used it how many times
@@ -112,16 +98,7 @@ const StudentNameIntentHandler = {
             repromptOutput = 'Please choose one of these courses: ' + availableCourses;
         }
 
-
-        if (repromptOutput === null) {
-            repromptOutput = speakOutput;
-        }
-
-        // Save state
-        persistentAttributes.repromptOutput = repromptOutput;
-        handlerInput.attributesManager.setPersistentAttributes(persistentAttributes);
-        await handlerInput.attributesManager.savePersistentAttributes();
-        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+        repromptOutput = await saveAttributes(speakOutput, repromptOutput, sessionAttributes, persistentAttributes, handlerInput);
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -160,14 +137,10 @@ const ChooseCourseIntentHandler = {
         persistentAttributes.currentCourse = course;
         
         // Get question
-        let {speakOutput, repromptOutput} = await startNewCourse(sessionAttributes, persistentAttributes, handlerInput);
+        let {speakOutput, repromptOutput} = await trainingHandler.startNewCourse(sessionAttributes, persistentAttributes, handlerInput);
         speakOutput = introOutput + " " + speakOutput;
         
-        // Save state
-        persistentAttributes.repromptOutput = repromptOutput;
-        handlerInput.attributesManager.setPersistentAttributes(persistentAttributes);
-        await handlerInput.attributesManager.savePersistentAttributes();
-        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+        repromptOutput = await saveAttributes(speakOutput, repromptOutput, sessionAttributes, persistentAttributes, handlerInput);
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -195,7 +168,7 @@ const ResumeCourseIntentHandler = {
             if (persistentAttributes.currentCourse !== null) {
                 // Able to resume
                 let introOutput = `Resuming course ${persistentAttributes.currentCourse}.`;
-                ({speakOutput, repromptOutput} = await startNewCourse(sessionAttributes, persistentAttributes, handlerInput));
+                ({speakOutput, repromptOutput} = await trainingHandler.startNewCourse(sessionAttributes, persistentAttributes, handlerInput));
                 speakOutput = introOutput + " " + speakOutput;
             } else {
                 speakOutput = `You have not started a course yet. Please choose a course first!`;
@@ -210,15 +183,7 @@ const ResumeCourseIntentHandler = {
             }
         }
         
-        if (repromptOutput === null) {
-            repromptOutput = speakOutput;
-        }
-
-        // Save state
-        persistentAttributes.repromptOutput = repromptOutput;
-        handlerInput.attributesManager.setPersistentAttributes(persistentAttributes);
-        await handlerInput.attributesManager.savePersistentAttributes();
-        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+        repromptOutput = await saveAttributes(speakOutput, repromptOutput, sessionAttributes, persistentAttributes, handlerInput);
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -241,16 +206,9 @@ const YesIntentHandler = {
         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
         const persistentAttributes = await handlerInput.attributesManager.getPersistentAttributes();
 
-        let {speakOutput, repromptOutput}  = await handleYesNoIntent(true, sessionAttributes, persistentAttributes, handlerInput);
-        if (repromptOutput === null) {
-            repromptOutput = speakOutput;
-        }
+        let {speakOutput, repromptOutput}  = await trainingHandler.handleYesNoIntent(true, sessionAttributes, persistentAttributes, handlerInput);
         
-        // Save state
-        persistentAttributes.repromptOutput = repromptOutput;
-        handlerInput.attributesManager.setPersistentAttributes(persistentAttributes);
-        await handlerInput.attributesManager.savePersistentAttributes();
-        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+        repromptOutput = await saveAttributes(speakOutput, repromptOutput, sessionAttributes, persistentAttributes, handlerInput);
         
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -270,16 +228,9 @@ const NoIntentHandler = {
         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
         const persistentAttributes = await handlerInput.attributesManager.getPersistentAttributes();
 
-        let {speakOutput, repromptOutput} = await handleYesNoIntent(false, sessionAttributes, persistentAttributes, handlerInput);
-        if (repromptOutput === null) {
-            repromptOutput = speakOutput;
-        }
+        let {speakOutput, repromptOutput} = await trainingHandler.handleYesNoIntent(false, sessionAttributes, persistentAttributes, handlerInput);
         
-        // Save state
-        persistentAttributes.repromptOutput = repromptOutput;
-        handlerInput.attributesManager.setPersistentAttributes(persistentAttributes);
-        await handlerInput.attributesManager.savePersistentAttributes();
-        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+        repromptOutput = await saveAttributes(speakOutput, repromptOutput, sessionAttributes, persistentAttributes, handlerInput);
 
         if (repromptOutput === -1) {
             // Stop the skill
@@ -302,7 +253,7 @@ const NumericAnswerIntentHandler = {
         let speakOutput = "Not implemented yet";
         let repromptOutput = null;
         
-        //const {speakOutput, repromptOutput} = await handleYesNoIntent(false);
+        //const {speakOutput, repromptOutput} = await trainingHandler.handleYesNoIntent(false);
         
         if (repromptOutput === null) {
             repromptOutput = speakOutput;
@@ -316,160 +267,19 @@ const NumericAnswerIntentHandler = {
 
 }
 
-// -------------------------------------------------------------------
-// Active training handler functions
-
-async function startNewCourse(sessionAttributes, persistentAttributes, handlerInput) {
-    persistentAttributes.startedTrainings += 1;
-    sessionAttributes.state = states.TRAINING;
-    sessionAttributes.questionNumber = 0;
-    sessionAttributes.score = 0;
-    return await getNextQuestion(sessionAttributes, persistentAttributes, handlerInput);
-}
-
-async function handleYesNoIntent(isYes, sessionAttributes, persistentAttributes, handlerInput) {
-    let speakOutput = null;
-    let repromptOutput = null;
-
-    if (sessionAttributes.state === states.TRAINING) {
-        // Update attributes
-        if (sessionAttributes.questionType !== 1) {
-            // We do not expect yes/no for this question type
-            speakOutput = "Your answer " + (isYes ? 'yes' : 'no') + " is not valid for this question. " + sessionAttributes.questionText;
-            repromptOutput = sessionAttributes.questionText;
-        } else {
-            // Repeat what the user said
-            let introOutput = "You said " + (isYes ? 'yes' : 'no') + ". ";
-            // Yes/No is a valid answer - check if correct.
-            introOutput += answerIsYesNoCorrect(isYes, sessionAttributes, persistentAttributes);
-            ({speakOutput, repromptOutput} = await getNextQuestion(sessionAttributes, persistentAttributes, handlerInput));
-            speakOutput = introOutput + " " + speakOutput;
-        }
-    } else if (sessionAttributes.state === states.FINISHED) {
-        if (isYes) {
-            let introOutput = `Restarting your course ${persistentAttributes.currentCourse}`;
-            ({speakOutput, repromptOutput} = await startNewCourse(sessionAttributes, persistentAttributes, handlerInput));
-            speakOutput = introOutput + " " + speakOutput;
-        } else {
-            // Finished trainingand user doesn't want to restart
-            speakOutput = "Thanks for training today. I hope I was able to help. Good bye!";
-            repromptOutput = -1;
-        }
-    } else {
-        // Not in training
-        // TODO: provide instructions on what to do
-        speakOutput = "You are currently not in training mode.";
-        if (persistentAttributes.repromptOutput !== null) {
-            speakOutput += ' ' + persistentAttributes.repromptOutput;
-        }
+async function saveAttributes(speakOutput, repromptOutput, sessionAttributes, persistentAttributes, handlerInput) {
+    if (repromptOutput === null) {
+        repromptOutput = speakOutput;
     }
 
-    return { speakOutput, repromptOutput };
+    // Save state
+    persistentAttributes.repromptOutput = repromptOutput;
+    handlerInput.attributesManager.setPersistentAttributes(persistentAttributes);
+    await handlerInput.attributesManager.savePersistentAttributes();
+    handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+    return repromptOutput;
 }
-
-async function getNextQuestion(sessionAttributes, persistentAttributes, handlerInput) {
-    let speakOutput = null;
-    let repromptOutput = null;
-
-    if (sessionAttributes.questionNumber >= 3) {
-        // Training finished
-        await trainingFinished(sessionAttributes, persistentAttributes, handlerInput);
-        speakOutput = `This training session is finished! You got a score of ${sessionAttributes.score}. You already finished ${persistentAttributes.finishedTrainings} trainings.`;
-        repromptOutput = `Would you like to train again?`;
-        speakOutput += " " + repromptOutput;
-    } else {
-        ({speakOutput, repromptOutput} = await getQuestionText(sessionAttributes, persistentAttributes, handlerInput));
-    }
-
-    return {speakOutput, repromptOutput};
-}
-
-// Function to retrieve the next question
-async function getQuestionText(sessionAttributes, persistentAttributes, handlerInput) {
-    console.log("I am in getQuestionText()");
-    let speakOutput = null;
-    let repromptOutput = null;
-
-    // Update session variables
-    sessionAttributes.questionNumber += 1;
-    persistentAttributes.totalQuestionsAsked += 1;
-
-    // Get question text
-    //const random = getRandom(0, data.length - 1);
-    //const questionDb = require(jsonFilePath);
-    let introText = `Question number ${sessionAttributes.questionNumber}: `;
-
-    // TODO: hardcoded question for now
-    let questionText = "Is accessibility only important for people with disabilities? Yes or no?";
-
-    // Store new question data
-    sessionAttributes.questionId = 1;
-    sessionAttributes.questionType = 1;
-    sessionAttributes.correctAnswer = 0;
-    sessionAttributes.questionText = questionText;
-
-    speakOutput = introText + questionText;
-    repromptOutput = questionText;
-
-    return {speakOutput, repromptOutput};
-}
-
-function answerIsYesNoCorrect(answerIsYes, sessionAttributes, persistentAttributes) {
-    if ((sessionAttributes.correctAnswer === 1 && answerIsYes) ||
-        (sessionAttributes.correctAnswer === 0 && !answerIsYes)) {
-        // Correct
-        return answerCorrect(sessionAttributes, persistentAttributes);
-    } else {
-        // Not correct
-        return answerWrong(sessionAttributes, persistentAttributes);
-    }
-}
-
-function answerCorrect(sessionAttributes, persistentAttributes) {
-    sessionAttributes.score += 1;
-    persistentAttributes.totalCorrectAnswers += 1;
-    let speakText = "Congratulations, the answer is correct!";
-    return speakText;
-}
-
-function answerWrong(sessionAttributes, persistentAttributes) {
-    persistentAttributes.totalWrongAnswers += 1;
-    let speakText = "Sorry, your answer was wrong.";
-    return speakText;
-}
-
-// -------------------------------------------------------------------
-// Training state handler functions
-function getJsonForCourse(courseName, sessionAttributes) {
-    return "data/" + sessionAttributes.courseName + ".json";
-}
-
-async function trainingFinished(sessionAttributes, persistentAttributes, handlerInput) {
-    sessionAttributes.state = states.FINISHED;
-    persistentAttributes.finishedTrainings += 1;
-}
-
-
-// -------------------------------------------------------------------
-// Custom utility functions
-function getRandom(min, max) {
-    return Math.floor((Math.random() * ((max - min) + 1)) + min);
-  }
-  
-
-// This function takes the contents of an array and randomly shuffles it.
-function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-  
-    while ( 0 !== currentIndex ) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
-  }
 
   
 // -------------------------------------------------------------------
---
+++ lambda/trainingHandler.js
@@ -0,0 +1,173 @@
+'use strict';
+
+// -------------------------------------------------------------------
+// Active training handler functions
+
+module.exports.initializeUser = function(sessionAttributes, persistentAttributes) {
+    persistentAttributes.currentCourse = null;
+    persistentAttributes.startedTrainings = 0;
+    persistentAttributes.finishedTrainings = 0;
+    persistentAttributes.totalQuestionsAsked = 0;
+    persistentAttributes.totalCorrectAnswers = 0;
+    persistentAttributes.totalWrongAnswers = 0;
+    sessionAttributes.state = states.STUDENT_NAME;
+}
+
+module.exports.startNewCourse = async function startNewCourse(sessionAttributes, persistentAttributes, handlerInput) {
+    persistentAttributes.startedTrainings += 1;
+    sessionAttributes.state = states.TRAINING;
+    sessionAttributes.questionNumber = 0;
+    sessionAttributes.score = 0;
+    return await getNextQuestion(sessionAttributes, persistentAttributes, handlerInput);
+}
+
+module.exports.handleYesNoIntent = async function handleYesNoIntent(isYes, sessionAttributes, persistentAttributes, handlerInput) {
+    let speakOutput = null;
+    let repromptOutput = null;
+
+    if (sessionAttributes.state === states.TRAINING) {
+        // Update attributes
+        if (sessionAttributes.questionType !== 1) {
+            // We do not expect yes/no for this question type
+            speakOutput = "Your answer " + (isYes ? 'yes' : 'no') + " is not valid for this question. " + sessionAttributes.questionText;
+            repromptOutput = sessionAttributes.questionText;
+        } else {
+            // Repeat what the user said
+            let introOutput = "You said " + (isYes ? 'yes' : 'no') + ". ";
+            // Yes/No is a valid answer - check if correct.
+            introOutput += answerIsYesNoCorrect(isYes, sessionAttributes, persistentAttributes);
+            ({speakOutput, repromptOutput} = await getNextQuestion(sessionAttributes, persistentAttributes, handlerInput));
+            speakOutput = introOutput + " " + speakOutput;
+        }
+    } else if (sessionAttributes.state === states.FINISHED) {
+        if (isYes) {
+            let introOutput = `Restarting your course ${persistentAttributes.currentCourse}`;
+            ({speakOutput, repromptOutput} = await module.exports.startNewCourse(sessionAttributes, persistentAttributes, handlerInput));
+            speakOutput = introOutput + " " + speakOutput;
+        } else {
+            // Finished trainingand user doesn't want to restart
+            speakOutput = "Thanks for training today. I hope I was able to help. Good bye!";
+            repromptOutput = -1;
+        }
+    } else {
+        // Not in training
+        // TODO: provide instructions on what to do
+        speakOutput = "You are currently not in training mode.";
+        if (persistentAttributes.repromptOutput !== null) {
+            speakOutput += ' ' + persistentAttributes.repromptOutput;
+        }
+    }
+
+    return { speakOutput, repromptOutput };
+}
+
+// -------------------------------------------------------------------
+// Private functions
+
+async function getNextQuestion(sessionAttributes, persistentAttributes, handlerInput) {
+    let speakOutput = null;
+    let repromptOutput = null;
+
+    if (sessionAttributes.questionNumber >= 3) {
+        // Training finished
+        await trainingFinished(sessionAttributes, persistentAttributes, handlerInput);
+        speakOutput = `This training session is finished! You got a score of ${sessionAttributes.score}. You already finished ${persistentAttributes.finishedTrainings} trainings.`;
+        repromptOutput = `Would you like to train again?`;
+        speakOutput += " " + repromptOutput;
+    } else {
+        ({speakOutput, repromptOutput} = await getQuestionText(sessionAttributes, persistentAttributes, handlerInput));
+    }
+
+    return {speakOutput, repromptOutput};
+}
+
+
+// Function to retrieve the next question
+async function getQuestionText(sessionAttributes, persistentAttributes, handlerInput) {
+    console.log("I am in getQuestionText()");
+    let speakOutput = null;
+    let repromptOutput = null;
+
+    // Update session variables
+    sessionAttributes.questionNumber += 1;
+    persistentAttributes.totalQuestionsAsked += 1;
+
+    // Get question text
+    //const random = getRandom(0, data.length - 1);
+    //const questionDb = require(jsonFilePath);
+    let introText = `Question number ${sessionAttributes.questionNumber}: `;
+
+    // TODO: hardcoded question for now
+    let questionText = "Is accessibility only important for people with disabilities? Yes or no?";
+
+    // Store new question data
+    sessionAttributes.questionId = 1;
+    sessionAttributes.questionType = 1;
+    sessionAttributes.correctAnswer = 0;
+    sessionAttributes.questionText = questionText;
+
+    speakOutput = introText + questionText;
+    repromptOutput = questionText;
+
+    return {speakOutput, repromptOutput};
+}
+
+
+function answerIsYesNoCorrect(answerIsYes, sessionAttributes, persistentAttributes) {
+    if ((sessionAttributes.correctAnswer === 1 && answerIsYes) ||
+        (sessionAttributes.correctAnswer === 0 && !answerIsYes)) {
+        // Correct
+        return answerCorrect(sessionAttributes, persistentAttributes);
+    } else {
+        // Not correct
+        return answerWrong(sessionAttributes, persistentAttributes);
+    }
+}
+
+function answerCorrect(sessionAttributes, persistentAttributes) {
+    sessionAttributes.score += 1;
+    persistentAttributes.totalCorrectAnswers += 1;
+    let speakText = "Congratulations, the answer is correct!";
+    return speakText;
+}
+
+function answerWrong(sessionAttributes, persistentAttributes) {
+    persistentAttributes.totalWrongAnswers += 1;
+    let speakText = "Sorry, your answer was wrong.";
+    return speakText;
+}
+
+
+
+// -------------------------------------------------------------------
+// Training state handler functions
+function getJsonForCourse(courseName, sessionAttributes) {
+    return "data/" + sessionAttributes.courseName + ".json";
+}
+
+async function trainingFinished(sessionAttributes, persistentAttributes, handlerInput) {
+    sessionAttributes.state = states.FINISHED;
+    persistentAttributes.finishedTrainings += 1;
+}
+
+
+// -------------------------------------------------------------------
+// Custom utility functions
+function getRandom(min, max) {
+    return Math.floor((Math.random() * ((max - min) + 1)) + min);
+  }
+  
+
+// This function takes the contents of an array and randomly shuffles it.
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while ( 0 !== currentIndex ) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
