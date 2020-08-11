e62257f4ee58b1f4701727ddd55ed3a69ca1d55b
Andreas Jakl <andreas.jakl@live.com>
2020-08-11 12:16:52 +0200

    Add further localization

--- lambda/i18n/en.js
+++ lambda/i18n/en.js
@@ -2,7 +2,8 @@ module.exports = {
     translation: {
         WELCOME: "Hi and welcome to the learning asssistant! I can help you understand the most important concepts of your courses. First, please tell me your first name!",
         WELCOME_REPROMPT: "Please tell me your first name.",
-        WELCOME_PERSONALIZED: "Welcome back <alexa:name type='first' personId='{{personId}}' />! <break /> {{prompt}}",
+        WELCOME_PERSONALIZED: "Welcome back {{personId}}! {{prompt}}",
+        //WELCOME_PERSONALIZED: "Welcome back <alexa:name type='first' personId='{{personId}}' />! <break /> {{prompt}}",
         WELCOME_PERSONALIZED_REPROMPT: "Would you like to resume your last course or start another course?",
         FALLBACK: "Sorry, I didn't catch that. Say that again please.", 
         FALLBACK_REPROMPT: "Say that again please.",
